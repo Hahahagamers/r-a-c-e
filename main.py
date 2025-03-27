1ce5e7d0676ff14bcdb42d67273a4111ee38a554
Hahahagamers <savir.senen@icloud.com>
2025-03-27 14:59:06 -0500

    Initial files for MakeCode project

---
+++ main.py
@@ -0,0 +1,4 @@
+game.splash("Hi", "")
+game.splash("Welcome... TO THE GAME YOU'VE BEEN WAITIN FOR!!!!")
+music.play(music.string_playable("B A G A G F A C5 ", 120),
+    music.PlaybackMode.UNTIL_DONE)
